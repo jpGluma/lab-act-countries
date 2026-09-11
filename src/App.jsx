a93574ca9ac5c1b294ddeae79aaeec78ad6d6a24
jpgluma <johnpaulaltillero@gmail.com>
2026-09-11 21:13:56 +0800

    Task 6: add 404 catch-all route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from "./pages/HomePage";
 import CountriesPage from "./pages/CountriesPage";
 import BucketListPage from "./pages/BucketListPage";
 import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 const App = () => {
   // TODO: replace this with a real route table using react-router.
   // For now the app only shows HomePage. Clicking nav links causes a full
@@ -15,6 +17,7 @@ const App = () => {
         <Route path="countries" element={<CountriesPage />} />
         <Route path="bucket-list" element={<BucketListPage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
